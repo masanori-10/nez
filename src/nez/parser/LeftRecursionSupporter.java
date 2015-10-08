7d1f77e8aabb448418bb4c11c3e09e11a99d3972
masanori-10 <s.masanori.vx@gmail.com>
2015-10-08 17:50:53 +0900

    made LeftRecursionSupporter stub
    first commit

---
+++ src/nez/parser/LeftRecursionSupporter.java
@@ -0,0 +1,23 @@
+package nez.parser;
+
+import java.util.HashMap;
+
+public class LeftRecursionSupporter {
+	private static HashMap<K, V>;
+
+}
+
+class M extends Instruction {
+	@Override
+	protected void encodeImpl(ByteCoder c) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public Instruction exec(RuntimeContext sc) throws TerminationException {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
