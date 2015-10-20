75bedc7c90572cf944d0734e67daf1d2467e2d02
masanori-10 <s.masanori.vx@gmail.com>
2015-10-20 19:07:55 +0900

    changed ILRCall, ILRGrow
    implemented main algorithm
    fix me ASTMachine bug

--- src/nez/parser/LeftRecursionSupporter.java
+++ src/nez/parser/LeftRecursionSupporter.java
@@ -1,41 +1,108 @@
 package nez.parser;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 import nez.lang.Expression;
 
-abstract class AbstractMapInstruction extends Instruction {
+abstract class AbstractLRInstraction extends Instruction {
 	protected static HashMap<String, HashMap<Long, MapEntry>> growing = new HashMap<String, HashMap<Long, MapEntry>>();
-	// protected static NonTerminal ruleOrig;
-	// protected static long posOrig;
 
-	AbstractMapInstruction(byte opcode, Expression e, Instruction next) {
+	public AbstractLRInstraction(byte opcode, Expression e, Instruction next) {
 		super(opcode, e, next);
+	}
+}
+
+class ILRCall extends AbstractLRInstraction {
+	ParseFunc f;
+	String name;
+	public Instruction jump = null;
+
+	private long pos;
+	private Object astlog;
+
+	ILRCall(ParseFunc f, String name, ILRGrow grow) {
+		super(InstructionSet.LRCall, null, grow);
+		this.f = f;
+		this.name = name;
+
+		if (!growing.containsKey(name)) {
+			growing.put(name, new HashMap<Long, MapEntry>());
+		}
+	}
+
+	ILRCall(ParseFunc f, String name, Instruction jump, ILRGrow grow) {
+		super(InstructionSet.LRCall, null, jump);
+		this.name = name;
+		this.f = f;
+		this.jump = grow;
+
+		if (!growing.containsKey(name)) {
+			growing.put(name, new HashMap<Long, MapEntry>());
+		}
+	}
+
+	void sync() {
+		if (this.jump == null) {
+			this.jump = labeling(this.next);
+			this.next = labeling(f.compiled);
+		}
+		this.f = null;
+	}
+
+	public final String getNonTerminalName() {
+		return this.name;
+	}
+
+	@Override
+	protected String getOperand() {
+		return label(jump);
 	}
 
 	@Override
 	protected void encodeImpl(ByteCoder c) {
 		// TODO encode argument
 	}
+
+	@Override
+	public Instruction exec(RuntimeContext sc) throws TerminationException {
+		this.pos = sc.getPosition();
+		this.astlog = sc.getAstMachine().saveTransactionPoint();
+		if (growing.get(this.name).containsKey(this.pos)) {
+			sc.setPosition(growing.get(this.name).get(this.pos).getPos());
+			if (growing.get(this.name).get(this.pos).getAnsType()) {
+				growing.get(this.name).get(this.pos).setLRDetected(true);
+				return sc.fail();
+			}
+			sc.getAstMachine().pasteTransactionPoint(growing.get(this.name).get(this.pos).getResult());
+			return ((ILRGrow) this.jump).jump;
+		}
+		growing.get(this.name).put(this.pos, new MapEntry(false, this.pos));
+		((ILRGrow) this.jump).pushPos(this.pos);
+		((ILRGrow) this.jump).pushAstlog(this.astlog);
+		StackData s = sc.newUnusedStack();
+		s.ref = this.jump;
+		return this.next;
+	}
 }
 
-class ILRCall extends AbstractMapInstruction {
+class ILRGrow extends AbstractLRInstraction {
 	ParseFunc f;
 	String name;
 	public Instruction jump = null;
-	private long pos;
 
-	ILRCall(ParseFunc f, String name, Instruction next) {
-		super(InstructionSet.LRCall, null, next);
+	private Stack<Long> pos = new Stack<Long>();
+	private Stack<Object> astlog = new Stack<Object>();
+	private boolean isGrow = false;
+
+	ILRGrow(ParseFunc f, String name, Instruction next) {
+		super(InstructionSet.LRGrow, null, next);
 		this.f = f;
 		this.name = name;
-		if (!growing.containsKey(name)) {
-			growing.put(name, new HashMap<Long, MapEntry>());
-		}
 	}
 
-	ILRCall(ParseFunc f, String name, Instruction jump, Instruction next) {
-		super(InstructionSet.LRCall, null, jump);
+	ILRGrow(ParseFunc f, String name, Instruction jump, Instruction next) {
+		super(InstructionSet.LRGrow, null, jump);
 		this.name = name;
 		this.f = f;
 		this.jump = next;
@@ -59,117 +126,58 @@ class ILRCall extends AbstractMapInstruction {
 	}
 
 	@Override
-	public Instruction exec(RuntimeContext sc) throws TerminationException {
-		this.pos = sc.getPosition();
-		if (!growing.get(this.name).containsKey(this.pos)) {
-			growing.get(this.name).put(this.pos, new MapEntry(false, this.pos));
-			StackData s = sc.newUnusedStack();
-			s.ref = this.jump;
-			return this.next;
-		} else {
-			sc.setPosition(growing.get(this.name).get(this.pos).getPos());
-			if (growing.get(this.name).get(this.pos).getAnsType()) {
-				growing.get(this.name).get(this.pos).setLRDetected(true);
-				return sc.fail();
-			} else {
-				sc.astMachine.rollTransactionPoint(growing.get(this.name).get(this.pos).getResult());
-				StackData s = sc.newUnusedStack();
-				s.ref = this.jump;
-				return this.next;
-			}
-		}
-	}// TODO add ILRPostCall
-}
+	protected void encodeImpl(ByteCoder c) {
+		// TODO encode argument
+	}
 
-// class ILRPreCall extends AbstractMapInstruction {
-// private NonTerminal rule;
-// private ILRPostCall post;
-//
-// ILRPreCall(NonTerminal rule, Instruction next, ILRPostCall post) {
-// super(InstructionSet.LRPreCall, null, next);
-// if (!growing.containsKey(rule)) {
-// growing.put(rule, new HashMap<Long, MapEntry>());
-// }
-// this.rule = rule;
-// this.post = post;
-// }
-//
-// @Override
-// public Instruction exec(RuntimeContext sc) throws TerminationException {
-// long pos = sc.getPosition();
-// this.post.setPos(pos);
-// this.post.setBase(sc.getAstMachine().saveTransactionPoint());
-// if (!growing.get(this.rule).containsKey(pos)) {
-// growing.get(this.rule).put(pos, new MapEntry(false, pos));
-// return this.next;
-// } else {
-// sc.setPosition(growing.get(this.rule).get(pos).getPos());
-// if (growing.get(this.rule).get(pos).getAnsType()) {
-// growing.get(this.rule).get(pos).setLRDetected(true);
-// return sc.fail();
-// } else {
-// sc.astMachine.rollTransactionPoint(growing.get(this.rule).get(pos).getResult());
-// return this.next;
-// }
-// }
-// }
-// }
-//
-// class ILRPostCall extends AbstractMapInstruction {
-// private NonTerminal rule;
-// private long pos;
-// private Object base;
-// private boolean isGrow = false;
-// private ICall jump;
-//
-// ILRPostCall(NonTerminal rule, Instruction next) {
-// super(InstructionSet.LRPostCall, null, next);
-// this.rule = rule;
-// }
-//
-// public void setJump(ICall jump) {
-// this.jump = jump;
-// }
-//
-// public void setPos(long pos) {
-// this.pos = pos;
-// }
-//
-// public void setBase(Object base) {
-// this.base = base;
-// }
-//
-// @Override
-// public Instruction exec(RuntimeContext sc) throws TerminationException {
-// if (this.isGrow) {
-// if (sc.getPosition() <= growing.get(this.rule).get(this.pos).getPos()) {
-// sc.setPosition(growing.get(this.rule).get(this.pos).getPos());
-// sc.getAstMachine().rollTransactionPoint(growing.get(this.rule).get(this.pos).getResult());
-// return this.next;
-// }
-// growing.get(this.rule).get(this.pos).setResult(sc.getAstMachine().saveTransactionPoint());
-// growing.get(this.rule).get(this.pos).setPos(sc.getPosition());
-// sc.setPosition(this.pos);
-// sc.getAstMachine().rollTransactionPoint(this.base);
-// return this.jump;
-// }
-// growing.get(this.rule).get(this.pos).setResult(sc.getAstMachine().saveTransactionPoint());
-// growing.get(this.rule).get(this.pos).setPos(sc.getPosition());
-// if (growing.get(this.rule).get(this.pos).getLRDetected() && this.pos !=
-// sc.getPosition()) {
-// sc.setPosition(this.pos);
-// sc.getAstMachine().rollTransactionPoint(this.base);
-// this.isGrow = true;
-// return this.jump;
-// }
-// return this.next;
-// }
-// }
+	public void pushPos(long pos) {
+		this.pos.push(pos);
+	}
+
+	public void pushAstlog(Object astlog) {
+		this.astlog.push(astlog);
+	}
+
+	@Override
+	public Instruction exec(RuntimeContext sc) throws TerminationException {
+		if (this.isGrow) {
+			if (sc.getPosition() <= growing.get(this.name).get(this.pos.peek()).getPos()) {
+				sc.setPosition(growing.get(this.name).get(this.pos.peek()).getPos());
+				sc.getAstMachine().backTransactionPoint(this.astlog.peek());
+				sc.getAstMachine().pasteTransactionPoint(growing.get(this.name).get(this.pos.peek()).getResult());
+				this.isGrow = false;
+				this.pos.pop();
+				this.astlog.pop();
+				return this.jump;
+			}
+			growing.get(this.name).get(this.pos.peek()).setPos(sc.getPosition());
+			growing.get(this.name).get(this.pos.peek()).setResult(sc.getAstMachine().getNextLog(this.astlog.peek()), sc.getAstMachine().saveTransactionPoint());
+			sc.setPosition(this.pos.peek());
+			sc.getAstMachine().backTransactionPoint(this.astlog.peek());
+			StackData s = sc.newUnusedStack();
+			s.ref = this;
+			return this.next;
+		}
+		growing.get(this.name).get(this.pos.peek()).setResult(sc.getAstMachine().getNextLog(this.astlog.peek()), sc.getAstMachine().saveTransactionPoint());
+		growing.get(this.name).get(this.pos.peek()).setPos(sc.getPosition());
+		if (growing.get(this.name).get(this.pos.peek()).getLRDetected() && this.pos.peek() < sc.getPosition()) {
+			sc.setPosition(this.pos.peek());
+			sc.getAstMachine().backTransactionPoint(this.astlog.peek());
+			StackData s = sc.newUnusedStack();
+			s.ref = this;
+			this.isGrow = true;
+			return this.next;
+		}
+		this.pos.pop();
+		this.astlog.pop();
+		return this.jump;
+	}
+}
 
 class MapEntry {
 	private boolean ansType; // true -> LR, false -> AST
 	private boolean lrDetected;
-	private Object result;
+	private Object[] result = new Object[2];
 	private long pos;
 
 	public MapEntry(boolean lrDetected, long pos) {
@@ -178,9 +186,10 @@ class MapEntry {
 		this.pos = pos;
 	}
 
-	public MapEntry(Object result, long pos) {
+	public MapEntry(Object first, Object end, long pos) {
 		this.ansType = false;
-		this.result = result;
+		this.result[0] = first;
+		this.result[1] = end;
 		this.pos = pos;
 	}
 
@@ -197,13 +206,14 @@ class MapEntry {
 		this.lrDetected = lrDetected;
 	}
 
-	public Object getResult() {
+	public Object[] getResult() {
 		return this.result;
 	}
 
-	public void setResult(Object result) {
+	public void setResult(Object first, Object end) {
 		this.ansType = false;
-		this.result = result;
+		this.result[0] = first;
+		this.result[1] = end;
 	}
 
 	public long getPos() {
