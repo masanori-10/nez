90db54f9cf92f2222d95bf66c5c45a18d7d5a2ea
masanori-10 <s.masanori.vx@gmail.com>
2015-11-16 14:42:12 +0900

    added DFA verbose view (ParsingTime)

--- src/nez/parser/ParsingMachine.java
+++ src/nez/parser/ParsingMachine.java
@@ -6,6 +6,7 @@ public class ParsingMachine {
 
 	public boolean run(Instruction code, SourceContext sc) {
 		boolean result = false;
+		long t = System.nanoTime();
 		try {
 			while (true) {
 				code = code.exec(sc);
@@ -13,6 +14,8 @@ public class ParsingMachine {
 		} catch (TerminationException e) {
 			result = e.status;
 		}
+		long t2 = System.nanoTime();
+		System.out.println("ParsingTime:" + (t2 - t));
 		return result;
 	}
 
